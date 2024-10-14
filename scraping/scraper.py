3acb3d6e25b7801abefd44f172c8140f7f2d9e42
LucasL. <lucaslagrimante@live.com>
2024-10-14 16:48:42 -0300

    Update scraper.py

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -6,15 +6,17 @@ def get_brawler_stats(map_name):
     # URL alvo com o nome do mapa. Modifique conforme o site de dados
     url = f"https://brawlify.com/maps/detail/{map_name}"
     response = requests.get(url)
+    
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         # Ajuste a lógica de scraping conforme a estrutura da página
         stats = []
-        for brawler in soup.select('#brawlers .justify-content-center .justify-content-center'):
-            id = brawler.select_one('.name').text
-            win_rate = brawler.select_one('.win-rate').text
-            use_rate = brawler.select_one('.use-rate').text
-            star_rate = brawler.select_one('.star-rate').text
+        brawler_elements = soup.find_all('div', class_='d-flex flex-column justify-content-center')
+        for brawler in brawler_elements:
+            id = brawler.find('img')['src'].split('/')[-1].replace('.png', '').strip()
+            win_rate = null
+            use_rate = null
+            star_rate = null
             
             stats.append({
                 'brawler': id,
